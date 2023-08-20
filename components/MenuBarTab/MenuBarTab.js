29a728b9b09c0fb807a8355556b8f5049004d43b
Ginver <74971813+Ginver@users.noreply.github.com>
2023-08-20 14:40:40 +0200

    MenuBarTab component temp deleted

---
+++ components/MenuBarTab/MenuBarTab.js
@@ -0,0 +1,33 @@
+// import React from "react";
+// import styles from "./MenuBarTab.module.css";
+
+// const MenuBarTab = ({ children, ...props }) => {
+//   return (
+//     <>
+//       <a>
+//         <div className={styles.menuBarTabUnit}>
+//           <div className={styles.planetTabUnit}>
+//             <div className={styles.planetTabColor}></div>
+//             <p {...props} className={styles.planetTabName}>
+//               {" "}
+//               {children}{" "}
+//             </p>
+//           </div>
+
+//           <div>
+//             <svg
+//               className={styles.iconErrow}
+//               xmlns="http://www.w3.org/2000/svg"
+//               width="6"
+//               height="8"
+//             >
+//               <path fill="none" stroke="#FFF" opacity=".4" d="M1 0l4 4-4 4" />
+//             </svg>
+//           </div>
+//         </div>
+//       </a>
+//     </>
+//   );
+// };
+
+// export default MenuBarTab;
