756dcd65bce7762cb6cc8a793f44bec9986447dc
Ginver <74971813+Ginver@users.noreply.github.com>
2023-09-25 14:11:33 +0200

    trzing to import Image as a comp but doesn't work

--- components/SourceLink/SourceLink.js
+++ components/SourceLink/SourceLink.js
@@ -1,13 +1,19 @@
 import React from "react";
 import styles from "./SourceLink.module.css";
 // import icon from "@/public/assets/icon-source.svg";
+import Image from "next/image";
 
 const SourceLink = ({ href }) => {
   return (
     <div className={styles.sectionSourceLink}>
       <p className={styles.source}>Source: </p>
       <a className={styles.sourceLink} href={href} target="_blank">
-        Wikipedia <img className={styles.icon} src="./assets/icon-source.svg" />
+        Wikipedia{" "}
+        <img
+          className={styles.icon}
+          src="./assets/icon-source.svg"
+          alt="errow"
+        />
       </a>
     </div>
   );
