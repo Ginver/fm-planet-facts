9470f3d6ce290cdcdc2262c4ff033dcc8a56bdd8
Ginver <74971813+Ginver@users.noreply.github.com>
2023-09-29 12:14:51 +0200

    handleclick funcn added

--- components/InfoBtns1/InfoBtns1.js
+++ components/InfoBtns1/InfoBtns1.js
@@ -1,17 +1,29 @@
 import React from "react";
 import styles from "./InfoBtns1.module.css";
 
-const InfoBtns1 = ({ children, ...props }) => {
+const InfoBtns1 = ({ children, handleclick, ...props }) => {
   return (
     <>
       <div {...props} className={styles.infoBtns1}>
-        <button {...props} className={styles.infoBtnsTitles}>
+        <button
+          {...props}
+          className={styles.infoBtnsTitles}
+          onClick={() => handleclick("Overview")}
+        >
           overview
         </button>
-        <button {...props} className={styles.infoBtnsTitles}>
+        <button
+          {...props}
+          className={styles.infoBtnsTitles}
+          onClick={() => handleclick("Structure")}
+        >
           structue
         </button>
-        <button {...props} className={styles.infoBtnsTitles}>
+        <button
+          {...props}
+          className={styles.infoBtnsTitles}
+          onClick={() => handleclick("Geology")}
+        >
           geology
         </button>
 
